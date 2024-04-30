0b6b635b29c7df16e7e81d5120eff548595204b7
alexbprr <alexbprr@gmail.com>
2024-04-30 00:42:53 -0300

    versao atual funcionando. modificacao da crate ode_solver para aceitar uma ref mutavel para o sistema de edos

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,30 @@
-mod core;
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
-use std::io::Write;
-use std::io::BufReader;
-use core::ParameterEstimation;
-use core::json::{load_json, save_json, Argument, Bound, ConfigData, Metadata};
+mod ode;
+use ode::ParameterEstimation;
+use ode::ga_json::{save_json, GA_Argument, Bound, ConfigData, GA_Metadata};
 
 use std::env;
 
-use crate::core::model::create_ode_system;
-use crate::core::model::State;
 fn main(){
     env::set_var("RUST_BACKTRACE", "1");
     
-    let metadata = Metadata {name: String::from("Parameter estimation"), start_time: 0.0, 
-        delta_time: 0.001, end_time: 10.0, population_size: 80, crossover_rate: 0.5, mutation_rate: 0.7};
+    let metadata = GA_Metadata {name: String::from("Parameter estimation"), start_time: 0.0, 
+        delta_time: 0.1, end_time: 60.0, population_size: 80, crossover_rate: 0.5, mutation_rate: 0.7};
     
-    let mut arguments: Vec<Argument> = vec![];
-    arguments.push(Argument::new(String::from("S"), 1000.0));
-    arguments.push(Argument::new(String::from("I"), 5.0));
-    arguments.push(Argument::new(String::from("R"), 0.0));
-    arguments.push(Argument::new(String::from("alpha"), 0.3));
-    arguments.push(Argument::new(String::from("beta"), 0.008));
-    arguments.push(Argument::new(String::from("gamma"), 0.15));
+    let mut arguments: Vec<GA_Argument> = vec![];
+    arguments.push(GA_Argument::new(String::from("N"), 1.));
+    arguments.push(GA_Argument::new(String::from("r"), 0.1));
+    arguments.push(GA_Argument::new(String::from("k"), 50.));
 
     let mut config_bounds = vec![];
-    config_bounds.push(Bound::new(String::from("alpha"),0.1, 1.0));
-    config_bounds.push(Bound::new(String::from("beta"),0.001, 0.1));
-    config_bounds.push(Bound::new(String::from("gamma"),0.1, 1.0));
+    config_bounds.push(Bound::new(String::from("r"),0.1, 1.0));
+    config_bounds.push(Bound::new(String::from("k"),1.0, 200.));
 
     let _ = save_json(ConfigData { metadata: metadata, arguments: arguments, bounds: config_bounds}, 
-            "./src/config/ga_input.json");
-                
-    let config_data = match load_json("./src/config/ga_input.json") {
-        Ok(config_model) => {println!("Config data: {:?}", config_model); config_model },
-        Err(e) => {println!("Error caused by {:?}", e); return},
-    };
-
-    let input_buffer: &mut String = &mut String::from("");
-    let file: File = match File::open("./src/tests/sir.txt") {
-        Ok(f) => f,
-        Err(e) => {println!("Error! {:?}", e); return;},
-    };
-    let mut reader: BufReader<File> = BufReader::new(file);
-    reader.read_to_string(input_buffer).unwrap();
-
-    //let mut ode_system = create_ode_system(String::from("I = beta*S*I - alpha*I \n R = alpha*I - gamma*R \n S = -beta*S*I + gamma*R"), &config_data);
-    let mut ode_system = create_ode_system(input_buffer.to_string(), &config_data);
-    println!("ODEs: {:#?}", ode_system);
-
-    let y: State = State::from_vec(ode_system.equations.keys()
-                    .map(|k| ode_system.get_argument_value(k.to_string())).collect());
-    //println!("{:#?}", ode_system.solve(y));
+            "./src/ode/config/ga_input.json");
     
-    let mut test: ParameterEstimation = ParameterEstimation::new( 
-        String::from("./src/tests/sir_data.csv"), config_data, 30);
-    test.estimate_parameters(&mut ode_system);
+    let mut param_estimator: ParameterEstimation = ParameterEstimation::new( 
+        String::from("./src/ode/tests/logistic_data.csv"), 50);
+    let mut ode_system = param_estimator.ode_system("./src/ode/config/ga_input.json", "./src/ode/tests/logistic.txt");
+    param_estimator.estimate_parameters(&mut ode_system);
     
 }
